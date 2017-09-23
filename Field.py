60201f58a143228375ac1cc86b020a358828076f
CorruptData <mccargishm@gmail.com>
2017-09-23 18:07:58 -0400

    Som more. Saving is hard.

--- Field.py
+++ Field.py
@@ -3,7 +3,8 @@ from Ball import Ball
 class Field:
 
   ball = Ball()
-  
+
   def __init__(self, players):
     self.players = players
     self.play_ongoing = False
+
--- GameState.py
+++ GameState.py
@@ -1,17 +1,23 @@
 from Field import Field
 from Clock import Clock
 from Team import Team
+import subprocess
 
 class GameState:
-  # Start in the first half
-  half = 1
 
   def __init__(self, halfsecs, ticktime):
 
+    # Start in the first half
+    self.half = 1
+    self.ticks_left = halfsecs/ticktime
+    # Create the teams from the rosters
     self.hometeam = Team("lua/roster1.txt")
     self.awayteam = Team("lua/roster2.txt")
     self.clock = Clock(halfsecs, ticktime)
     self.field = Field()
+    self.down = 1
+    self.to_go = 10
+    self.yardline = 20
 
   def update(self, commands):
     # Update the field
@@ -19,6 +25,7 @@ class GameState:
 
     # Tick the Clock
     self.clock.update()
+    self.ticks_left-=1
     # Mom~, is it over yet?
     if self.clock.time <= 0:
       if self.half == 1:
@@ -45,6 +52,21 @@ class GameState:
     elif action == 'FG':
       self.fieldgoal()
     '''
+  def get_down(self):
+    return str(self.down)
+  
+  def get_to_go(self):
+    return str(self.to_go)
+
+  def get_to_td(self):
+    return str(100-self.yardline)
+
+  def get_ticks_left(self):
+    return str(ticks_left)
+
+  def get_half(self):
+    return str(half)
+
 
 gs = GameState(300.0, 0.1)
 
@@ -56,7 +78,25 @@ while True:
       for line in rone:
         sone.write(line)
     sone.write("\n")
-    sone.write(gs.field.get_down())
+    sone.write(gs.field.get_down()+",DOWN\n")
+    sone.write(gs.field.get_to_go()+",TOGO\n")
+    sone.write(gs.field.get_to_td()+",TOTD\n")
+    sone.write(gs.field.get_ticks_left()+",TICK\n")
+    sone.write(gs.field.get_half()+",HALF\n")
 
+  with open('lua/state2.txt') as stwo:
+    stwo.write("DECLARE DEFENSE\n\n")
+    with open('lua/roster2.txt') as rtwo:
+      for line in rtwo:
+        stwo.write(line)
+    stwo.write("\n")
+    stwo.write(gs.field.get_down()+",DOWN\n")
+    stwo.write(gs.field.get_to_go()+",TOGO\n")
+    stwo.write(gs.field.get_to_td()+",TOTD\n")
+    stwo.write(gs.field.get_ticks_left()+",TICK\n")
+    stwo.write(gs.field.get_half()+",HALF\n")
 
+  subprocess.call()
   gs.update()
+
+
