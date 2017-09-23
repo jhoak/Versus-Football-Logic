4ffb39160e6ad18b46e0155d210e19a953a5a1db
WJLIDDY <wjl36@case.edu>
2017-09-23 19:02:53 -0400

    Outputs results

--- lua/dumb/run.lua
+++ lua/dumb/run.lua
@@ -10,17 +10,17 @@ for key,value in pairs(game_state["players"]) do
 end
 
 if game_state["status"] == "DECLARE OFFENSE" or game_state["status"] == "DECLARE DEFENSE" then
-  commands["C"] = players[1]
-  commands["LG"] = players[2]
-  commands["RG"] = players[3]
-  commands["LT"] = players[4]
-  commands["RT"] = players[5]
-  commands["R1"] = {players[6],-500,3000}
-  commands["R2"] = {players[7],-500,4000}
-  commands["R3"] = {players[8],-500,7000}
-  commands["R4"] = {players[9],-500,-3000}
-  commands["R5"] = {players[10],-500,-4000}
-  commands["QB"] = {players[11],5000}
+  commands[players[1]] = {"C"}
+  commands[players[2]] = {"LG"}
+  commands[players[3]] = {"RG"}
+  commands[players[4]] = {"LT"}
+  commands[players[5]] = {"RT"}
+  commands[players[6]] = {"R",-500,3000}
+  commands[players[7]] = {"R",-500,4000}
+  commands[players[8]] = {"R",-500,7000}
+  commands[players[9]] = {"R",-500,-3000}
+  commands[players[10]] = {"R",-500,-4000}
+  commands[players[11]] = {"QB", 5000}
 end
 
 print("Done!")
--- lua/run_ai.lua
+++ lua/run_ai.lua
@@ -2,21 +2,35 @@
 local state = require('readstate')
 local inspect = require('inspect')
 
+function save_results()
+  -- operates on "commands" local
+  -- Write 'SET' commands
+  -- dude n^2 lmao
+  io.output("result.txt","w")
+  for key,value in pairs(env.commands) do
+    -- linemen set commands
+    if(value[1] == "C" or value[1] == "LG" or value[1] == "RG" or value[1] == "LT" or value[1] == "RT") then
+      io.write("SET ", value[1], " ", key, "\n")
+    end
+  end
+end
+
 function ai_timer()
-  print(env.commands)
+  if os.clock() - start_time > 0.04 then
+    print "Time is up!"
+    save_results(env.commands)
+    os.exit()
+  end;
 end
 -- Pass state as arg1 and pass team folder as arg 2
 function run_ai()
-  
+
+-- Load gamestate
   local game_state_l = state.readstate(arg[1])
 
---
+-- Init Env
   local passtable = {}
   passtable["insert"] = table.insert
-
-
-  --print(inspect(game_state_l))
-
   env = {
     print = print,
     pairs = pairs,
@@ -25,12 +39,14 @@ function run_ai()
     commands = {}
   }
 
+-- Compile Code
   loaded_ai = loadfile(arg[2] .. "/run.lua","t",env)
 -- Start the clock.
+  start_time  = os.clock()
   debug.sethook(ai_timer, "l")
--- Run.
 -- TODO: if this is nil, then it didn't compile.
   loaded_ai()
+  save_results()
 end
 
 run_ai()
