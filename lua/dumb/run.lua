e8578767eadb932122bc8e47eb8103d37bebdc3d
WJLIDDY <wjl36@case.edu>
2017-09-23 18:06:28 -0400

    lua:

--- lua/dumb/run.lua
+++ lua/dumb/run.lua
@@ -1,4 +1,26 @@
-function run(game_state,output)
-  print "touchdown"
+-- The dumb ai (tm)--
+players = {}
+
+-- Pick players from roster randomly 
+for key,value in pairs(game_state["players"]) do
+  table.insert(players,value["number"])
+  if #table == 11 then
+    break
+  end
 end
-print "outside lmao"
+
+if game_state["status"] == "DECLARE OFFENSE" or game_state["status"] == "DECLARE DEFENSE" then
+  commands["C"] = players[1]
+  commands["LG"] = players[2]
+  commands["RG"] = players[3]
+  commands["LT"] = players[4]
+  commands["RT"] = players[5]
+  commands["R1"] = {players[6],-500,3000}
+  commands["R2"] = {players[7],-500,4000}
+  commands["R3"] = {players[8],-500,7000}
+  commands["R4"] = {players[9],-500,-3000}
+  commands["R5"] = {players[10],-500,-4000}
+  commands["QB"] = {players[11],5000}
+end
+
+print("Done!")
--- lua/run_ai.lua
+++ lua/run_ai.lua
@@ -3,24 +3,34 @@ local state = require('readstate')
 local inspect = require('inspect')
 
 function ai_timer()
-  --print(string.format("%.2f\n", os.clock()))
+  print(env.commands)
 end
 -- Pass state as arg1 and pass team folder as arg 2
 function run_ai()
-  print(arg[1])
-  print(arg[2])
+  
+  local game_state_l = state.readstate(arg[1])
 
--- Run gamestate
-  local game_state = state.readstate(arg[1])
--- Compile function, text only, in jail.
-  local env = {} 
+--
+  local passtable = {}
+  passtable["insert"] = table.insert
+
+
+  --print(inspect(game_state_l))
+
+  env = {
+    print = print,
+    pairs = pairs,
+    table = passtable,
+    game_state = game_state_l,
+    commands = {}
+  }
+
   loaded_ai = loadfile(arg[2] .. "/run.lua","t",env)
 -- Start the clock.
   debug.sethook(ai_timer, "l")
 -- Run.
+-- TODO: if this is nil, then it didn't compile.
   loaded_ai()
-  loaded_ai.run(game_state,output)
-
 end
 
 run_ai()
