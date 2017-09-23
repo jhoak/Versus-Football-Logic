f0f2f25a7918bb40d83e43c6ab0deef64e665d40
WJLIDDY <wjl36@case.edu>
2017-09-23 15:13:10 -0400

    Adding lua folder

---
+++ lua/readstate.lua
@@ -0,0 +1,115 @@
+local inspect = require('inspect')
+
+function split(inputstr, sep)
+  local t={} ; i=1
+  for str in string.gmatch(inputstr, "([^"..sep.."]+)") do
+          t[i] = str
+          i = i + 1
+  end
+  return t
+end
+
+
+function readstate(infile)
+  io.input(infile)
+  local state_type = io.read("*line")
+  -- read a newline
+  io.read("*line")
+  local output = {}
+
+  if state_type == "DECLARE OFFENSE" then
+    print("Declare Offense")
+    output["players"] = read_team_players()
+  end
+
+  if state_type == "DECLARE DEFENSE" then
+    print("Declare Defense")
+    output["opponent"] = read_opposing_players()
+    output["players"] = read_team_players()
+  end
+
+  if state_type == "MOVE OFFENSE" then
+    print("Move Offense")
+    output["players"] = read_team_players()
+    output["opponent"] = read_opposing_players()
+    output["ball"] = read_ball
+  end
+  
+  if state_type == "MOVE DEFENSE" then
+    print("Move Defense")
+    output["opponent"] = read_opposing_players()
+    output["players"] = read_team_players()
+    output["ball"] = read_ball
+  end
+
+
+  return output
+end
+
+function game_state()
+  local game = {}
+  game["down"] = split(io.read("*line"), ",")[0]
+  game["togo"] = split(io.read("*line"), ",")[0]
+  game["totd"] = split(io.read("*line"), ",")[0]
+  game["tick"] = split(io.read("*line"), ",")[0]
+  game["half"] = split(io.read("*line"), ",")[0]
+  return game
+end
+
+function read_team_players()
+  local players = {}
+  local player = io.read("*line")
+
+  while (player ~= "") do
+    local stats = split(player, ",")
+    local player_table = {}
+    player_table["name"] = stats[1]
+    player_table["number"] =  stats[2]
+    player_table["speed"] = stats[3]
+    player_table["hit"] =  stats[4]
+    player_table["kick"] = stats[5]
+    player_table["disp"] =  stats[6]
+    player_table["recv"] = stats[7]
+    player_table["pass"] =  stats[8]
+    player_table["x"] =  stats[9]
+    player_table["y"] =  stats[10]
+    player = io.read("*line")
+  end
+  return players
+end
+
+function read_opposing_players()
+  local players = {}
+  local player = io.read("*line")
+
+  while (player ~= "") do
+    local stats = split(player, ",")
+    local player_table = {}
+    player_table["name"] = stats[1]
+    player_table["number"] =  stats[2]
+    player_table["x"] =  stats[3]
+    player_table["y"] =  stats[4]
+    player = io.read("*line")
+    table.insert(players,player_table)
+  end
+  return players
+end
+
+function read_ball()
+  local ball = split(io.read("*line")," ")
+  io.read("*line")
+  if #ball == 2 then
+    return ball[2]
+  else
+    local vector = {}
+    vector["x"] = ball[2]
+    vector["y"] = ball[3]
+    vector["z"] = ball[4]
+    return vector
+  end
+end
+
+print(inspect(readstate("state1.txt")))
+print(inspect(readstate("state2.txt")))
+print(inspect(readstate("state3.txt")))
+print(inspect(readstate("state4.txt")))
