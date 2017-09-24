a2d49685ae776220189987f3a784f272232ef728
CorruptData <mccargishm@gmail.com>
2017-09-23 23:58:33 -0400

    State 3 nearly ready

--- python/Team.py
+++ python/Team.py
@@ -3,11 +3,14 @@ import os, sys
 
 class Team:
 
-  def __init__(self, teamname, home_or_away, roster):
+  def __init__(self, teamname, home_or_away, roster, ai):
     # Get players from file
     with open(roster) as p:
       self.name = teamname
       self.side = home_or_away
+      self.roster = roster
+      self.ai = ai
+      self.score = 0
 
       self.Players = []
 
