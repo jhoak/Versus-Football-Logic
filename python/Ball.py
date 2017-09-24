d05604c8a3ef5d60b9f85944e5b6eec98a1a4635
CorruptData <mccargishm@gmail.com>
2017-09-24 03:53:56 -0400

    Ball can be thrown

--- python/Ball.py
+++ python/Ball.py
@@ -1,4 +1,5 @@
 from Player import Player
+import math
 
 class Ball:
   # Position
@@ -10,20 +11,34 @@ class Ball:
   # Passed
   passed = False
 
+  gravity = 10.72835
+
   def __init__(self):
     pass
 
   def setvector(self, x, y, z):
-    self.held = False
     self.x_speed = x
     self.y_speed = y
     self.z_speed = z
 
+  def throw(self, x, y):
+    self.held = False
+    dist = Math.sqrt(math.pow(x-self.x, 2)+math.pow(y-self.y, 2))
+    vel = math.sqrt(dist/self.gravity) * self.gravity
+    self.z_speed = (math.sqrt(2)*vel)/2
+    self.x_speed = ((math.sqrt(2)*vel)/2) * math.atan(math.abs(y-self.y)/math.abs(x-self.x))
+    self.y_speed = self.z_speed-self.x_speed
+
+
   def update(self):
-    if held:
-      # TODO
-      pass
-    x += x_speed
+    if not held:
+      self.x += self.x_speed
+      self.y += self.y_speed
+      self.z += self.z_speed
+      self.z_speed -= self.gravity
+      if self.z <= 0:
+        self.setvector(0,0,0)
+        self.z = 0
 
   def player_hold(self, player):
     self.held = player
