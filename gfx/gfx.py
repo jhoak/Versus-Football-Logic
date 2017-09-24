217c9347cfe7b1131eb11f7ac3a2e150979bc727
WJLIDDY <wjl36@case.edu>
2017-09-24 12:39:08 -0400

    Better GFX

--- gfx/gfx.py
+++ gfx/gfx.py
@@ -4,7 +4,7 @@ from os.path import isfile, join
 
 # quick and dirty gameplay display
 def read_offense_file(i):
-	fname = "gameframes/state" + str(100+i) +".txt"
+	fname = "../lua/vid/state" + str(100+i) +".txt"
 	if not isfile(fname):
 		return False 
 
@@ -15,6 +15,10 @@ def read_offense_file(i):
 		d["o"+str(i+1)] = [int(lines[i+2].split(",")[-2]),int(lines[i+2].split(",")[-1])]
 		d["d"+str(i+1)] = [int(lines[i+14].split(",")[-2]),int(lines[i+14].split(",")[-1])]
 
+		# Map number to index.
+		d["onum"+lines[i+2].split(",")[1]] = i+1
+		d["dnum"+lines[i+14].split(",")[1]] = i+1
+
 	d["down"] = lines[28].split(",")[0]
 	d["togo"] = int(lines[29].split(",")[0])
 	d["totd"] = int(lines[30].split(",")[0])
@@ -24,8 +28,10 @@ def read_offense_file(i):
 
 	if(len(lines[26].split(",")) == 7):
 		d["ballvec"] = [int(lines[26].split(",")[1]), int(lines[26].split(",")[2])]
+		print("vector")
 	else:
 		d["ballplayer"] = [lines[26].split(",")[1], lines[26].split(",")[2]]
+		print("grid")
 	return d
 # Converts all offensive gamestate files in /gfx/tmp
 def to_movie():
@@ -49,10 +55,8 @@ def to_movie():
 		draw.line((center[0], 0, center[0], 200), fill="blue")
 		draw.line((fd, 0, fd, 200), fill="yellow")
 
-		if "ballvec" in f:
-			vec = f["ballvec"]
-			ball_pos = [int(vec[0]/100) + center[0] +-3, int(vec[1]/100) + center[1]+-3]
-			frame.paste(ball,ball_pos)
+		ball_y= None
+		ball_carrier = None
 
 		for player in range(11):
 			d_coord = f["d" +str(player+1)]
@@ -64,21 +68,43 @@ def to_movie():
 			frame.paste(o,d_px)
 			frame.paste(x,o_px)
 
+		if "ballvec" in f:
+			vec = f["ballvec"]
+			print(vec)
+			ball_pos = [int(vec[0]/100) + center[0] +-3, int(vec[1]/100) + center[1]+-3]
+			frame.paste(ball,ball_pos)
+
+		if "ballplayer" in f:
+			tup = f["ballplayer"]
+			ball_carrier = f[tup[0]+"num"+tup[1].rstrip()]
+			print(ball_carrier)
+			coord = f[tup[0]+str(ball_carrier)]
+			print(coord)
+			px = [int(coord[0]/100) + center[0] +-3, int(coord[1]/100) + center[1]+-3]
+			frame.paste(ball,px)
+
+
+
+		to_draw = Image.new("RGB", [200,200])
+		print(center)
+		to_draw.paste(frame,(-center[0]+100,0))
+		draw = ImageDraw.Draw(to_draw)
+
 		# get a font
-		fnt = ImageFont.truetype('font.ttf', 10)
+		fnt = ImageFont.truetype('font.ttf', 5)
 
-		# draw text, half opacity
-		draw.text((10,10), "Offense:" + f["offense_score"], font=fnt, fill=(0,0,0,255))
-		# draw text, full opacity
-		draw.text((10,30), "Defense:" + f["defense_score"] , font=fnt, fill=(0,0,0,255))
+		draw.text((10,150), "Down: " + f["down"] , font=fnt, fill=(0,0,0,255))
 
-		draw.text((10,70), "Down:" + f["down"] , font=fnt, fill=(0,0,0,255))
+		draw.text((110,150), "Time: " + str(int(f["tick"] / (600))) + ":" + str(int((f["tick"] % 600)/10)).zfill(2), font=fnt, fill=(0,0,0,255))
 
-		draw.text((10,90), "Time:" + str(int(f["tick"] / (600))) + ":" + str(int((f["tick"] % 600)/10)), font=fnt, fill=(0,0,0,255))
+		draw.text((10,170), "Offense:" + f["offense_score"], font=fnt, fill=(0,0,0,255))
 
-		frame.save('tmp/' + str(i).zfill(5) + '.png')
+		draw.text((110,170), "Defense:" + f["defense_score"] , font=fnt, fill=(0,0,0,255))
+
+		to_draw.save('tmp/' + str(i).zfill(5) + '.png')
 		i += 1
 
 	system('ffmpeg -f image2 -r 10 -i tmp/%05d.png -vcodec mpeg4 -y movie.mp4')
+
 
 to_movie()
